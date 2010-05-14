f69a23439ad74e473366ed44c007b4a7842cee34
Dan Mohl <dmohl@yahoo.com>
2010-05-14 17:19:16 -0500

    Ported the HomeController to F#

---
+++ src/FSharpMVC2.Controllers/HomeController.fs
@@ -0,0 +1,16 @@
+﻿namespace FSharpMVC2.Web.Controllers
+
+open System
+open System.Collections.Generic
+open System.Web
+open System.Web.Mvc
+
+[<HandleError>]
+type HomeController =
+    inherit Controller
+        new () = {inherit Controller()}
+        member x.Index () : ActionResult =
+            x.ViewData.["Message"] <- "Welcome to ASP.NET MVC!"
+            x.View() :> ActionResult
+        member x.About () =
+            x.View() :> ActionResult
