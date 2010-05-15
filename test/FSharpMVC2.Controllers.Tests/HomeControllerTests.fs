c37e3b87c1e386f305d625107a589bfc5007d89e
Dan Mohl <dmohl@yahoo.com>
2010-05-15 12:13:20 -0500

    Made the tests pass and added a few more tests

--- test/FSharpMVC2.Controllers.Tests/HomeControllerTests.fs
+++ test/FSharpMVC2.Controllers.Tests/HomeControllerTests.fs
@@ -9,11 +9,27 @@ open System.Web.Mvc
 
 [<TestFixture>]      
 type HomeController__when_calling_the_index_action () =   
+    let SUT = new HomeController()
     [<DefaultValue(false)>]  
-    val mutable result : string
+    val mutable result : ViewResult
     inherit SpecUnit.ContextSpecification()
         override x.Because () =
-            x.result <- ""
+            x.result <- SUT.Index() :?> ViewResult
+        [<Test>]    
+        member x.should_have_a_non_null_result () =    
+            x.result.ShouldNotBeNull |> ignore
         [<Test>]    
         member x.should_have_the_expected_message_in_view_data () =    
-            x.result.ShouldEqual("Welcome to ASP.NET MVC!") |> ignore
+            x.result.ViewData.["Message"].ShouldEqual("Welcome to ASP.NET MVC!") |> ignore
+
+[<TestFixture>]      
+type HomeController__when_calling_the_about_action () =   
+    let SUT = new HomeController()
+    [<DefaultValue(false)>]  
+    val mutable result : ViewResult
+    inherit SpecUnit.ContextSpecification()
+        override x.Because () =
+            x.result <- SUT.About() :?> ViewResult
+        [<Test>]    
+        member x.should_have_a_non_null_result () =    
+            x.result.ShouldNotBeNull |> ignore
